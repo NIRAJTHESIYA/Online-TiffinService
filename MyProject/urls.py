a4228d428fbe6664f0737673ed3852acee6229db
NIRAJTHESIYA <153067974+NIRAJTHESIYA@users.noreply.github.com>
2024-08-19 22:18:44 +0530

    Add files via upload

---
+++ MyProject/urls.py
@@ -0,0 +1,44 @@
+"""
+URL configuration for MyProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from Tiffin import views
+from django.conf import settings
+from django.conf.urls.static import static
+from Tiffin.views import *
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('header/', views.header, name='header'),
+    path('footer/', views.footer, name='footer'),
+    path('meal/', views.meal, name='meal'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('reg/', views.reg, name='reg'),
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('profile/', views.profile, name='profile'), 
+    path('update_profile/', views.update_profile, name='update_profile'), 
+    path('logout/', views.logout, name='logout'),
+    path('terms/', views.terms, name='terms'),
+    path('privacy/', views.privacy, name='privacy'),   
+    path('menu/', views.menu, name='menu'),  
+     
+    
+]
